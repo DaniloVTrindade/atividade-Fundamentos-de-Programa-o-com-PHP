dab91a18ca2331ffcb01cb4a242216c78dfd5280
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-08-28 22:40:22 -0300

    Adiciona questão 4

---
+++ questao4.php
@@ -0,0 +1,14 @@
+<?php
+$numero1 = floatval(readline("Digite o primeiro número: "));
+$numero2 = floatval(readline("Digite o segundo número: "));
+
+$soma = $numero1 + $numero2;
+$subtracao = $numero1 - $numero2;
+$multiplicacao = $numero1 * $numero2;
+$divisao = $numero1 / $numero2;
+
+echo "Soma: $soma\n";
+echo "Subtração: $subtracao\n";
+echo "Multiplicação: $multiplicacao\n";
+echo "Divisão: $divisao\n";
+?>
