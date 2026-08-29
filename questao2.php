34af4d7d0b700fcd3ef8e01ba37d0c38de1112ad
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-08-28 22:40:12 -0300

    Adiciona questão 2

---
+++ questao2.php
@@ -0,0 +1,8 @@
+<?php
+$numero1 = floatval(readline("Digite o primeiro número: "));
+$numero2 = floatval(readline("Digite o segundo número: "));
+
+$soma = $numero1 + $numero2;
+
+echo "A soma é: $soma\n";
+?>
