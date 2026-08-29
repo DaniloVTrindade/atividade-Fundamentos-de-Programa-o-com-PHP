5c4833681e89124a83571c0545a37a220cbbfb2e
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-08-28 22:40:32 -0300

    Adiciona questão 6

---
+++ questao6.php
@@ -0,0 +1,9 @@
+<?php
+$numero1 = floatval(readline("Digite o primeiro número: "));
+$numero2 = floatval(readline("Digite o segundo número: "));
+$numero3 = floatval(readline("Digite o terceiro número: "));
+
+$media = ($numero1 + $numero2 + $numero3) / 3;
+
+echo "A média aritmética é: $media\n";
+?>
