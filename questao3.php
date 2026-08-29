5a59a01047ccc8d318784d25a5adbc86e124378f
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-08-28 22:40:16 -0300

    Adiciona questão 3

---
+++ questao3.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = floatval(readline("Digite o primeiro número: "));
+$numero2 = floatval(readline("Digite o segundo número: "));
+
+$subtracao = $numero1 - $numero2;
+$multiplicacao = $numero1 * $numero2;
+
+echo "Subtração: $subtracao\n";
+echo "Multiplicação: $multiplicacao\n";
+?>
